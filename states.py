fba0de2dbc8aa41a35d4d27e6c84f9714c35acda
Julian Klemm <julian.klemm1@uni-hamburg.de>
2023-11-08 11:09:58 +0100

    updated so that notImplemented is raised, otherwise the with no implementation at all the app fails since the setup process takes longer than the app takes to run

--- states.py
+++ states.py
@@ -1,5 +1,5 @@
 from FeatureCloud.app.engine.app import AppState, app_state
-
+import time
 
 # FeatureCloud requires that apps define the at least the 'initial' state.
 # This state is executed after the app instance is started.
@@ -7,7 +7,13 @@ from FeatureCloud.app.engine.app import AppState, app_state
 class InitialState(AppState):
 
     def register(self):
-        self.register_transition('terminal')  # We declare that 'terminal' state is accessible from the 'initial' state.
+        self.register_transition('terminal')  
+        # We declare that 'terminal' state is accessible from the 'initial' state.
 
     def run(self):
-        return 'terminal'  # This means we are done. If the coordinator transitions into the 'terminal' state, the whole computation will be shut down.
+        raise NotImplemented # remove this when you finnish implementation
+        # Checkout our documentation for help on how to implement an app
+        # https://featurecloud.ai/assets/developer_documentation/getting_started.html
+        return 'terminal'  
+        # This means we are done. If the coordinator transitions into the 
+        # 'terminal' state, the whole computation will be shut down.
